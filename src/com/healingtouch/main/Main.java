ceb54aeb4664e346ea62c7ab1b63567f65e6996a
Desktop MB <Desktop MB@DESKTOP-9TOMG64>
2019-07-07 18:15:21 -0500

    New UI setup

--- src/com/healingtouch/main/Main.java
+++ src/com/healingtouch/main/Main.java
@@ -1,4 +1,4 @@
-package com.healingtouch.poli.main;
+package com.healingtouch.main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,28 +7,27 @@ import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
-
 public class Main extends Application {
-	
+
 	public static Stage stage = null;
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/com/healingtouch/poli/view/Reports.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/com/healingtouch/view/Doctor.fxml"));
 			Scene scene = new Scene(root);
 			primaryStage.initStyle(StageStyle.UNDECORATED);
 			primaryStage.setScene(scene);
 			Main.stage = primaryStage;
 			stage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * launchApp
-	 * */
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
